ef2aee25d55fb6859bc24ad013b80913a1f63ebb
aliu.popov <nomail@fakemail.ru>
2024-11-29 10:26:35 +0300

    Добавил описание задания и вольную реализацию

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,13 @@
-def __caesarify__(dictionary, key, text):
+def __caesarify__(dictionary, key, text, decode=False):
     power = len(dictionary)
     map = {} 
     
     for i in range(len(dictionary)):
         y = (i + key) % power 
-        map[dictionary[i]] = dictionary[y]
+        if not decode:
+            map[dictionary[i]] = dictionary[y]
+        else:
+            map[dictionary[y]] = dictionary[i]
     
     result = '' 
     for i in range(len(text)):
@@ -17,20 +20,44 @@ def __caesarify__(dictionary, key, text):
     
     return result
 
-def caesarify_en(text, key):
-    dictionary = 'abcdefghijklmnopqrstuvwxyz'
-    return __caesarify__(dictionary, key, text)  
-    
-def caesarify_ru(text, key):
-    dictionary = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-    return __caesarify__(dictionary, key, text)  
+dictionary_en = 'abcdefghijklmnopqrstuvwxyz'
+dictionary_ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
-#text = input('Введите текст: ')
+def encode_en(text, key):
+    return __caesarify__(dictionary_en, key, text)
+
+def decode_en(text, key):
+    return __caesarify__(dictionary_en, key, text, decode=True)		  
+    
+def encode_ru(text, key):
+    return __caesarify__(dictionary_ru, key, text)  
+
+def decode_ru(text, key):
+    return __caesarify__(dictionary_ru, key, text, decode=True)      
+
 text = 'Съешь же ещё этих мягких французских булок да выпей чаю.'
-print(caesarify_ru(text, 3))
+print(encode_ru(text, 3))
 #Фэзыя йз зьи ахлш пвёнлш чугрщцкфнлш дцосн жг еютзм ъгб.
 
+text = 'Фэзыя йз зьи ахлш пвёнлш чугрщцкфнлш дцосн жг еютзм ъгб.'
+print(decode_ru(text, 3))
+#Съешь же ещё этих мягких французских булок да выпей чаю.
+
 text = 'The quick brown fox jumps over the lazy dog.'
-print(caesarify_en(text, -3))
+print(encode_en(text, -3))
 #Qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald.
+
+text = 'Qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald.'
+print(decode_en(text, -3))
+#The quick brown fox jumps over the lazy dog.
+
+
+#Требуется реализовать две функции для работы с шифром Цезаря.
+#Напишите функцию encode(s, rotn), которая принимает два аргумента: исходный текст и значение сдвига, выполняет кодирование и возвращает закодированный текст.
+#Напишите функцию decode(s, rotn), которая принимает два аргумента: закодированный текст и значение сдвига, выполняет декодирование и возвращает оригинальный текст.
+#Кодируются только буквы алфавита, числа, символы пунктуации и другие символы не кодируются.
+#Сдвиг - это целое число, он цикличен и его максимальное значение равно количеству букв алфавита. В задаче используется только латинский алфавит, поэтому значение сдвига может быть от 0 до 25 (всего 26 символов).
+#Нужно учитывать регистр символов, a со смещением 1, возвращает b, а A - возвращает B.
+#В решении задачи вам пригодятся строковые функции/методы вашего ЯП, а также функции для работы с ASCII-кодами символов.
+#Требуется реализовать только функцию, решение не должно осуществлять операций ввода-вывода.
 
